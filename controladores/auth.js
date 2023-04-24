a55e379ddb4ac737d1dcd776e399e9bd110828fd
rocady21 <rodrigooliverman12@gmail.com>
2023-04-24 12:25:26 -0300

    Update onsole.log auth.js

--- controladores/auth.js
+++ controladores/auth.js
@@ -1,24 +1,24 @@
-const {request,response} = require("express");
+const { request, response } = require("express");
 const Usuario = require("../modelsBD/Usuario.js")
-const {generarJWT} = require("../middlware/generarJWT.js")
+const { generarJWT } = require("../middlware/generarJWT.js")
 // libreria para encriptar contraseña 
 const bcrypt = require("bcrypt")
 
 
 
 
-const CrearUsuario = async(req = request,res = response) => {
-    
-        const {photo,name,email,password,rol} = req.body
-        
+const CrearUsuario = async (req = request, res = response) => {
+
+    const { photo, name, email, password, rol } = req.body
+
     try {
 
-        let usuario = await Usuario.findOne({email:email})
+        let usuario = await Usuario.findOne({ email: email })
 
-        if(usuario) {
+        if (usuario) {
             res.status(400).json({
-                ok:false,
-                msg:"ya hay un usuario con ese correo"
+                ok: false,
+                msg: "ya hay un usuario con ese correo"
             })
         }
         // instanciar el usuario al schema de la bd
@@ -26,69 +26,70 @@ const CrearUsuario = async(req = request,res = response) => {
 
         // encriptar contraseña 
         const salt = bcrypt.genSaltSync();
-        usuario.password = bcrypt.hashSync(password,salt)
+        usuario.password = bcrypt.hashSync(password, salt)
         // guardar usuario en base de datos
         await usuario.save()
 
         // generar JWT
         const token = await generarJWT(usuario.name, usuario._id);
-        
 
-         res.status(200).json({
-         ok:true,
-         message: "Usuario Creado",
-         name: usuario.name,
-         uid: usuario._id,
-         rol: usuario.rol,
-         photo:usuario.photo,
-         token : token    
-    })        
+        console.log("HOLA")
+
+        res.status(200).json({
+            ok: true,
+            message: "Usuario Creado",
+            name: usuario.name,
+            uid: usuario._id,
+            rol: usuario.rol,
+            photo: usuario.photo,
+            token: token
+        })
     } catch (error) {
         console.log(error)
         res.status(400).json({
-            ok:false,
-            msg:"Por favor hable con el administrador"
+            ok: false,
+            msg: "Por favor hable con el administrador"
         })
     }
 
 }
-const LoginUsuario = async(req,res = response)=> {
+const LoginUsuario = async (req, res = response) => {
 
-        const {email,password} = req.body
+    const { email, password } = req.body
     try {
         // evaluar si el email existe con find
-        const usuario = await Usuario.findOne({email:email})
+        const usuario = await Usuario.findOne({ email: email })
 
-        if(!usuario) {
+        if (!usuario) {
             res.status(404).json({
-                ok:false,
+                ok: false,
                 msg: "no hay un usuario con ese correo"
             })
         }
 
         //evaluar si la contraseña es la misma y existe
-        const validPassword = bcrypt.compareSync(password,usuario.password)
+        const validPassword = bcrypt.compareSync(password, usuario.password)
 
-        if(!validPassword) {
+        if (!validPassword) {
             return res.status(404).json({
-                ok:false,
+                ok: false,
                 msg: "password incorrecto"
             })
         }
 
-        
+
 
         // generar jwt 
-        const token = await generarJWT(usuario.id,usuario.name)
+        const token = await generarJWT(usuario.id, usuario.name)
 
         res.status(200).json({
             ok: true,
             msg: "Login correctoo",
-            uid : usuario._id,
+            uid: usuario._id,
             name: usuario.name,
             rol: usuario.rol,
-            photo:usuario.photo,
-            token : token
+            photo: usuario.photo,
+            token: token
 
         })
 
@@ -98,61 +99,61 @@ const LoginUsuario = async(req,res = response)=> {
     } catch (error) {
         console.log(error);
         res.status(400).json({
-            ok:false,
+            ok: false,
             msg: "por favor hable con el administrador"
         })
-        
+
     }
-    
+
 }
-const RevalidarJWT = async(req,res = response)=> {
-    
+const RevalidarJWT = async (req, res = response) => {
+
     const name = req.name
     const uid = req.uid
 
 
-    const token = await generarJWT(name,uid);
+    const token = await generarJWT(name, uid);
 
     res.status(200).json({
-        ok:true,
-        token ,
+        ok: true,
+        token,
         uid,
         name,
 
     })
 
 }
-const ListadoUsuarios = (req,res = response)=> {
-    
+const ListadoUsuarios = (req, res = response) => {
+
     const usuarios = Usuario.find()
     try {
-        if(usuarios) {
+        if (usuarios) {
             res.status(200).json({
-                ok:true,
-                 usuarios
+                ok: true,
+                usuarios
             })
         }
     } catch (error) {
         console.log(error);
         res.status(400).json({
-            ok:false,
+            ok: false,
             msg: "por favor hable con el administrador xd"
         })
-        
+
     }
 }
 
-const getInfoByToken = async (req,res = response)=> {
-    
+const getInfoByToken = async (req, res = response) => {
+
     const uid = req?.uid;
 
     console.log(uid)
 
-    const userInfo = await Usuario.findOne({_id: uid});
+    const userInfo = await Usuario.findOne({ _id: uid });
     try {
-        if(userInfo) {
+        if (userInfo) {
             res.status(200).json({
-                ok:true,
+                ok: true,
                 userInfo
             })
         } else {
@@ -160,10 +161,10 @@ const getInfoByToken = async (req,res = response)=> {
         }
     } catch (error) {
         res.status(400).json({
-            ok:false,
+            ok: false,
             msg: error?.message,
         })
-        
+
     }
 }
 
